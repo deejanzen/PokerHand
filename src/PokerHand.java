820bc406cd6ca4860462a7117be64bb6c1b0caff
deejanzen <djanzen@email.arizona.edu>
2016-01-22 13:33:30 -0700

    pair/kicker

--- src/PokerHand.java
+++ src/PokerHand.java
@@ -9,10 +9,11 @@ import java.util.Stack;
 public class PokerHand {
     private List<Card> hand;
     private HandRank handRank;
-    private List<Card> Kicker;
+    private List<Card> kicker;
 
     public PokerHand(Card a, Card b, Card c, Card d, Card e){
         hand = new ArrayList<>();
+        kicker = new ArrayList<>();
         Stack<Card> temp = new Stack<>();
         temp.push(a);
         temp.push(b);
@@ -26,21 +27,41 @@ public class PokerHand {
         }
 
         Collections.sort(hand);
+
         //if you have a pair then check the pair based hands
         if ( hand.get(0).getRank().getValue() == hand.get(1).getRank().getValue() ||
              hand.get(1).getRank().getValue() == hand.get(2).getRank().getValue() ||
              hand.get(2).getRank().getValue() == hand.get(3).getRank().getValue() ||
              hand.get(3).getRank().getValue() == hand.get(4).getRank().getValue()     ){
 
+            //pp*** where */=*
+            if (hand.get(0).getRank().getValue() == hand.get(1).getRank().getValue() &&
+                hand.get(2).getRank().getValue() != hand.get(3).getRank().getValue() &&
+                hand.get(3).getRank().getValue() != hand.get(4).getRank().getValue()    ){
+
+                    handRank = HandRank.OnePair;
+                    kicker.add(2, hand.get(2));
+                    kicker.add(3, hand.get(3));
+                    kicker.add(4, hand.get(4));
+            }
         }
 
         //otherwise...
         else {
-
+            return;
         }
 
+
+
+    }//end constructor
+
+    public HandRank getHandRank(){
+        return handRank;
+    }
+    public List<Card> getKicker(){
+        return kicker;
     }
 
 
 
-}
+}//end PokerHand
