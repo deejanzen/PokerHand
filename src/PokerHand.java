4b5dc425a984a6aecae019fa4c4d7d8581d00cbe
deejanzen <djanzen@email.arizona.edu>
2016-01-22 09:44:45 -0700

    started rank chcking in constructor

--- src/PokerHand.java
+++ src/PokerHand.java
@@ -8,6 +8,7 @@ import java.util.Stack;
  */
 public class PokerHand {
     private List<Card> hand;
+    private HandRank handRank;
     private List<Card> Kicker;
 
     public PokerHand(Card a, Card b, Card c, Card d, Card e){
@@ -25,7 +26,21 @@ public class PokerHand {
         }
 
         Collections.sort(hand);
+        //if you have a pair then check the pair based hands
+        if ( hand.get(0).getRank().getValue() == hand.get(1).getRank().getValue() ||
+             hand.get(1).getRank().getValue() == hand.get(2).getRank().getValue() ||
+             hand.get(2).getRank().getValue() == hand.get(3).getRank().getValue() ||
+             hand.get(3).getRank().getValue() == hand.get(4).getRank().getValue()     ){
+
+        }
+
+        //otherwise...
+        else {
+
+        }
 
     }
+
+
 
 }
